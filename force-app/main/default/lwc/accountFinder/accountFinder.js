291ff6a18f2fd647248ad6164a6ef9f206a8523e
Desu Desu <desudesu410@gmail.com>
2021-06-29 20:22:49 +0300

    feat: add method in accListController for accFinder component

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,5 +1,5 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 export default class AccountFinder extends LightningElement {
   annualRevenue = null;
 
@@ -9,5 +9,8 @@ export default class AccountFinder extends LightningElement {
 
   reset() {
     this.annualRevenue = null;
-  }  
+  } 
+  
+  @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue'})
+  accounts;
 }
